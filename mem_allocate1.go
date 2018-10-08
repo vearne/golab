b0992a9ccb3fda921e9cb1dbd0b44fa0184f1893
vearne <asdwoshiaotian@gmail.com>
2018-10-08 12:06:02 +0800

    update mem_allocate2.go

--- mem_allocate1.go
+++ mem_allocate1.go
@@ -4,16 +4,30 @@ import(
 	"github.com/imroc/req"
 	"fmt"
 	"runtime"
+	"net/http"
+	"time"
 )
 
+func SetConnPool() {
+	client := &http.Client{}
+	client.Transport = &http.Transport{
+		MaxIdleConnsPerHost: 1000,
+		// 无需设置MaxIdleConns
+		// MaxIdleConns controls the maximum number of idle (keep-alive)
+		// connections across all hosts. Zero means no limit.
+		// MaxIdleConns 默认是0，0表示不限制
+	}
+
+	req.SetClient(client)
+	req.SetTimeout(5 * time.Second)
+}
 
 func init(){
 	SetConnPool()
 }
 
 func main(){
-	size := 1
-
+	size := 0
 
 	var err error
 	var resp *req.Resp
@@ -23,18 +37,20 @@ func main(){
 		resp, err = req.Get(url)
 		if err!= nil{
 			fmt.Println("error", err)
+			continue
 		}
-		fmt.Println(resp.ToString())
-		if 1 % 10000 == 0{
+		data,  _ := resp.ToBytes()
+		fmt.Println("resp byte length", len(data))
+		if i % 10000 == 0{
 			runtime.ReadMemStats(&mem)
 			fmt.Println("---------------", i)
-			fmt.Println(mem.Alloc)
-			fmt.Println(mem.TotalAlloc)
+			fmt.Println("mem.Alloc", mem.Alloc)
+			fmt.Println("mem.TotalAlloc", mem.TotalAlloc)
 			// 堆对象占用的字节大小
-			fmt.Println(mem.HeapAlloc)
-			fmt.Println(mem.HeapSys)
+			fmt.Println("mem.HeapAlloc", mem.HeapAlloc)
+			fmt.Println("mem.HeapSys", mem.HeapSys)
 			// 分配在堆上的对象数量
-			fmt.Println(mem.HeapObjects)
+			fmt.Println("mem.HeapObjects", mem.HeapObjects)
 		}
 	}
 
--- mem_allocate2.go
+++ mem_allocate2.go
@@ -1,22 +1,37 @@
 package main
 
-import(
+import (
 	"github.com/imroc/req"
 	"fmt"
 	"runtime"
 	"sync"
 	"bytes"
+	"net/http"
+	"time"
 )
+
+func SetConnPool() {
+	client := &http.Client{}
+	client.Transport = &http.Transport{
+		MaxIdleConnsPerHost: 1000,
+		// 无需设置MaxIdleConns
+		// MaxIdleConns controls the maximum number of idle (keep-alive)
+		// connections across all hosts. Zero means no limit.
+		// MaxIdleConns 默认是0，0表示不限制
+	}
+
+	req.SetClient(client)
+	req.SetTimeout(5 * time.Second)
+}
 
 var buffPool sync.Pool
 
-func init(){
+func init() {
 	SetConnPool()
 }
 
-func main(){
-	size := 1
-
+func main() {
+	size := 0
 
 	var err error
 	var resp *req.Resp
@@ -24,34 +39,40 @@ func main(){
 	var byteSlice []byte
 	url := "http://up.xiaorui.cc:9000/bigjson.json"
 	var mem runtime.MemStats
-	for i:=0;i<size + 1;i++{
+	for i := 0; i < size+1; i++ {
 		resp, err = req.Get(url)
-		if err!= nil{
+		if err != nil {
 			fmt.Println("error", err)
+			continue
 		}
 
 		item := buffPool.Get()
-		if item == nil{
-			byteSlice = make([]byte, 10 * 1024)
-		}else{
+		if item == nil {
+			byteSlice = make([]byte, 0, 10*1024)
+		} else {
 			byteSlice = item.([]byte)
 		}
 		buffer = bytes.NewBuffer(byteSlice)
 		buffer.ReadFrom(resp.Response().Body)
-		fmt.Println(buffer.String())
 
+
+		res  := buffer.Bytes()
+		fmt.Println("resp byte length", len(res))
+
+		fmt.Println("len", buffer.Len())
+		fmt.Println("Cap", buffer.Cap())
 		buffPool.Put(byteSlice)
 
-		if 1 % 10000 == 0{
+		if i%10000 == 0 {
 			runtime.ReadMemStats(&mem)
 			fmt.Println("---------------", i)
-			fmt.Println(mem.Alloc)
-			fmt.Println(mem.TotalAlloc)
+			fmt.Println("mem.Alloc", mem.Alloc)
+			fmt.Println("mem.TotalAlloc", mem.TotalAlloc)
 			// 堆对象占用的字节大小
-			fmt.Println(mem.HeapAlloc)
-			fmt.Println(mem.HeapSys)
+			fmt.Println("mem.HeapAlloc", mem.HeapAlloc)
+			fmt.Println("mem.HeapSys", mem.HeapSys)
 			// 分配在堆上的对象数量
-			fmt.Println(mem.HeapObjects)
+			fmt.Println("mem.HeapObjects", mem.HeapObjects)
 		}
 	}
 
