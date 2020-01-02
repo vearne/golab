247e677bc115174adfd80a32df5e628e78f3b0c6
vearne <asdwoshiaotian@gmail.com>
2020-01-02 11:36:10 +0800

    update master

--- zaplog/log_test.go
+++ zaplog/log_test.go
@@ -5,24 +5,24 @@ import (
 	"testing"
 )
 
-//func BenchmarkZapFile(b *testing.B) {
-//	logger := InitLogger()
-//	defer logger.Sync()
-//	b.ResetTimer()
-//	for i := 0; i < b.N; i++ {
-//		logger.Info("hello world!")
-//	}
-//}
-//
-//func BenchmarkZapStdout(b *testing.B) {
-//	logger := InitLogger2()
-//	defer logger.Sync()
-//	fmt.Println()
-//	b.ResetTimer()
-//	for i := 0; i < b.N; i++ {
-//		logger.Info("hello world!")
-//	}
-//}
+func BenchmarkZapFile(b *testing.B) {
+	logger := InitLogger()
+	defer logger.Sync()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("hello world!")
+	}
+}
+
+func BenchmarkZapStdout(b *testing.B) {
+	logger := InitLogger2()
+	defer logger.Sync()
+	fmt.Println()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("hello world!")
+	}
+}
 
 func BenchmarkAsync(b *testing.B) {
 	logger := InitLogger3()
